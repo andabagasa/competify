ebafc5746923d8644e7cb614458f16fb3ecca9c6
Anda Bagas Aprianto <andabagasa21@gmail.com>
2023-11-16 12:12:50 +0700

    fix bug v2.0

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -11,12 +11,12 @@
         </a>
         <img src="{{ asset('assets/images/register.png') }}" alt="" class="hidden md:block h-[480px] w-auto">
     </div>
-    <div class="p-12 flex flex-col gap-6 bg-white rounded-2xl shadow w-full md:w-[360px]">
+    <div class="p-12 flex flex-col gap-6 bg-white rounded-2xl shadow">
         <h1 class="text-5xl font-bold">Buat Akun</h1>
-        <form action="/register" method="post" class="flex flex-col gap-1">
+        <form action="/register" method="post" class="w-full md:w-[360px] flex flex-col gap-1">
             @csrf
-            <label for="nama" class="label-input">Nama</label>
-            <input type="text" name="nama" id="nama" placeholder="Masukkan nama anda" class="input-field">
+            <label for="name" class="label-input">Nama</label>
+            <input type="text" name="name" id="name" placeholder="Masukkan nama anda" class="input-field">
             <label for="email" class="label-input mt-3">Email</label>
             <input type="email" name="email" id="email" placeholder="Masukkan email anda" class="input-field">
             <label for="no_tlp" class="label-input mt-3">Nomor Whatsapp</label>
