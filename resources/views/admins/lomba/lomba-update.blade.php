d5c697a804e11642b922562801658f37c9224933
Anda Bagas Aprianto <andabagasa21@gmail.com>
2023-11-18 14:38:14 +0700

    test dashboard lomba

--- resources/views/admins/lomba/lomba-update.blade.php
+++ resources/views/admins/lomba/lomba-update.blade.php
@@ -3,5 +3,26 @@
 @section('title', 'Update Lomba')
 
 @section('content')
-
+<div>
+    <h1>Tambah Lomba</h1>
+    <form action="" method="post" class="flex flex-col gap-4">
+        <label for="name" class="label-input">Nama</label>
+        <input type="text" name="name" id="name" placeholder="Masukkan nama anda" class="input-field">
+        <label for="email" class="label-input mt-3">Deskripsi</label>
+        <input type="email" name="email" id="email" placeholder="Masukkan email anda" class="input-field">
+        <label for="no_tlp" class="label-input mt-3">Penyelenggara</label>
+        <input type="text" name="no_tlp" id="no_tlp" placeholder="Masukkan nomor whatsapp anda" class="input-field">
+        <label for="no_tlp" class="label-input mt-3">Open Registrasi</label>
+        <input type="date" name="no_tlp" id="no_tlp" placeholder="Masukkan nomor whatsapp anda" class="input-field">
+        <label for="no_tlp" class="label-input mt-3">Close Registrasi</label>
+        <input type="date" name="no_tlp" id="no_tlp" placeholder="Masukkan nomor whatsapp anda" class="input-field">
+        <label for="no_tlp" class="label-input mt-3">Reward</label>
+        <input type="text" name="no_tlp" id="no_tlp" placeholder="Masukkan nomor whatsapp anda" class="input-field">
+        <label for="no_tlp" class="label-input mt-3">Syarat & Ketentuan</label>
+        <input type="text" name="no_tlp" id="no_tlp" placeholder="Masukkan nomor whatsapp anda" class="input-field">
+        <label for="no_tlp" class="label-input mt-3">Poster Lomba</label>
+        <input type="file" name="no_tlp" id="no_tlp" placeholder="Masukkan nomor whatsapp anda" class="input-field">
+        <button type="submit" class="btn-primary-normal mt-5">Buat Akun</button>
+    </form>
+</div>
 @endsection
--- resources/views/admins/lomba/lomba.blade.php
+++ resources/views/admins/lomba/lomba.blade.php
@@ -3,5 +3,41 @@
 @section('title', 'Dashboard Lomba')
 
 @section('content')
-
+<div class="flex flex-col items-center justify-center gap-12 mt-12">
+    <h1 class="text-4xl font-lora font-bold">Daftar Lomba</h1>
+    <table class="table-fixed text-center">
+        <thead>
+        <tr>
+            <th class="border p-4 border-collapse border-primary-500 w-[200px] bg-primary-500 text-primary-50">Poster</th>
+            <th class="border p-4 border-collapse border-primary-500 w-[200px] bg-primary-500 text-primary-50">Nama Lomba</th>
+            <th class="border p-4 border-collapse border-primary-500 w-[200px] bg-primary-500 text-primary-50">Deskripsi</th>
+            <th class="border p-4 border-collapse border-primary-500 w-[200px] bg-primary-500 text-primary-50">Penyelenggara</th>
+            <th class="border p-4 border-collapse border-primary-500 w-[200px] bg-primary-500 text-primary-50">Reward</th>
+            <th class="border p-4 border-collapse border-primary-500 w-[400px] bg-primary-500 text-primary-50">Timeline</th>
+            <th class="border p-4 border-collapse border-primary-500 w-[400px] bg-primary-500 text-primary-50">Syarat & Ketentuan</th>
+        </tr>
+        </thead>
+        <tbody>
+            {{-- @foreach ($menus as $menu)
+            <tr class="">
+                <td class="border p-2  border-collapse border-primary-500">{{ $menu->name }}</td>
+                <td class="border p-2 border-collapse border-primary-500">{{ $menu->category->name }}</td>
+                <td class="border p-2 border-collapse border-primary-500 w-[200px]">{{ $menu->description }}</td>
+                <td class="border p-2 border-collapse border-primary-500">{{ $menu->price }}</td>
+                <td class="border p-2 border-collapse border-primary-500">
+                    <img class="ml-auto mr-auto" src="{{ asset('storage/' . $menu->image) }}" alt="{{ $menu->name }}" width="50" height="50">
+                </td>
+                <td class="border p-2 border-collapse border-primary-500">
+                <a class="text-base font-semibold text-primary-500 hover:text-primary-300 hover:duration-300 mx-8" href="/admin/menu/update/{{ $menu->id }}">
+                        Update
+                    </a>
+                    <a class="text-base font-semibold text-primary-500 hover:text-primary-300 hover:duration-300 mx-8" href="/admin/menu/delete/{{ $menu->id }}" data-confirm-delete="true">
+                        Hapus
+                    </a>
+                </td>
+            </tr>
+            @endforeach --}}
+        </tbody>
+    </table>
+</div>
 @endsection
